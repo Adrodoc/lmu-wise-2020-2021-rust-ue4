d9c3fbe725bf454d6086d6703f241bcd6420b9ea
Adrodoc <adrodoc55@googlemail.com>
2020-12-01 14:37:04 +0100

    custom more compact formating

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,18 @@ const SEPERATOR: &str = "seperator";
 
 fn main() {
     let matches = App::new("ue4")
-        .arg(
-            Arg::with_name(TEXT)
-                .short("t")
-                .long("text")
-                .help("The text to split using the seperator")
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name(SEPERATOR)
-                .short("s")
-                .long("sep")
-                .help("The seperator to use for splitting the text")
-                .takes_value(true)
-                .required(true),
-        )
+        .arg(Arg::with_name(TEXT)
+            .short("t")
+            .long("text")
+            .help("The text to split using the seperator")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name(SEPERATOR)
+            .short("s")
+            .long("sep")
+            .help("The seperator to use for splitting the text")
+            .takes_value(true)
+            .required(true))
         .get_matches();
     let text = matches.value_of(TEXT).unwrap();
     let seperator = matches.value_of(SEPERATOR).unwrap();
@@ -29,9 +25,6 @@ fn main() {
     println!("{:#?}", map);
 }
 
-fn split<'l>(text: &'l str, seperator: &str) -> BTreeMap<usize, &'l str> {
-    text.split(seperator)
-        .filter(|element| element.len() != 0)
-        .map(|element| (element.as_ptr() as usize - text.as_ptr() as usize, element))
-        .collect()
+fn split<'l>(t: &'l str, s: &str) -> BTreeMap<usize, &'l str> {
+    t.split(s).filter(|e| e.len() != 0).map(|e| (e.as_ptr() as usize - t.as_ptr() as usize, e)).collect()
 }
